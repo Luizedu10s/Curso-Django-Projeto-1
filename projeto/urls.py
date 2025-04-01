f7129b1440eba1e72ffb6fe9e49b61105ee51cf8
LuizEdu10s <luiz-souza.ls@academico.ifpb.edu.br>
2025-03-31 22:03:57 -0300

    Direcionando rotas no Django

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+def my_view(request):
+    return HttpResponse("Hello, world!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', my_view),
 ]
