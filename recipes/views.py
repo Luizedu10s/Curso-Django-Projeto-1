1f28c79094e4b3b8be6fcac90d33335d7442f6aa
LuizEdu10s <luiz-souza.ls@academico.ifpb.edu.br>
2025-04-05 07:30:37 -0300

    Alterações

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 
+from django.http import HttpResponse
+from django.shortcuts import render
 # Crie suas views aqui.
 
 def my_view(request):
     return HttpResponse("Hello, world!")
 
 def home(request):
-    return HttpResponse("Welcome to the home page!")
+    return render(request, 'recipes/home.html')
 
 def about(request):
     return HttpResponse("This is the about page.")
